bd0689348b731271d368577e4e92f80f7139673f
Onish Mistry <onish.mistry@1rivet.com>
2017-04-15 11:04:36 +0530

    World changed to Onish

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace cmdtool
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Onish!");
             Console.ReadKey();
 
         }
