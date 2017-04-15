751c61ac7cad4b2bec73db18f053ffe6ee196245
Onish Mistry <onish.mistry@1rivet.com>
2017-04-15 11:27:15 +0530

    Added Timestamp

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace cmdtool
         {
             
             var str = Console.ReadLine();
-            Console.WriteLine("Hello: " + str);
+            Console.WriteLine(DateTime.Now.ToString() + " Hello: " + str);
 
             Console.ReadKey();
 
